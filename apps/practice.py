f1533d8104fc3acbe2ce753c05fadd18b1154fc8
Dilkush Singh <work.dilkushsingh@gmail.com>
2024-11-27 16:47:28 +0530

    fix:handled try except

--- apps/practice.py
+++ apps/practice.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import streamlit as st
 import time
-from constant import PRACTICE_TITLE, IDEAL_POSE_PATH, ANGLE_THRESHOLD, USER_MSG, UPLOAD_ISSUE, CAMERA_NOT_DETECTED_ISSUE
+from constant import PRACTICE_TITLE, IDEAL_POSE_PATH, ANGLE_THRESHOLD, USER_MSG, UPLOAD_ISSUE, CAMERA_NOT_DETECTED_ISSUE, CAMERA_ISSUE, POSE_NOT_DETECTED_ISSUE
 from utils.pose_utils import Comparison
 from utils.streamlit_helpers import fetch_image
 
@@ -41,40 +41,44 @@ class PosePractice:
                 st.error(UPLOAD_ISSUE)
 
     def practice_by_feed(self):
-        col1, col2 = st.columns(2)
+        try:
+            col1, col2 = st.columns(2)
 
-        with col1:
-            ideal_frame_path = self.fetch_ideal_pose()
-            ideal_frame = self.s3utils.fetch_image(ideal_frame_path)
+            with col1:
+                ideal_frame_path = self.fetch_ideal_pose()
+                ideal_frame = self.s3utils.fetch_image(ideal_frame_path)
+                if ideal_frame is not None:
+                    st.image(ideal_frame, channels='BGR', use_column_width=True)
+                st.info(USER_MSG)
+
             if ideal_frame is not None:
-                st.image(ideal_frame, channels='BGR', use_column_width=True)
-            st.info(USER_MSG)
+                with col2:
+                    start_camera = st.button("Start Camera", key="start")
+                    stop_camera = st.button("Stop Camera", key="stop")
 
-        if ideal_frame is not None:
-            with col2:
-                start_camera = st.button("Start Camera", key="start")
-                stop_camera = st.button("Stop Camera", key="stop")
+                    if start_camera and not stop_camera:
+                        self.cap = cv2.VideoCapture(0)
+                        time.sleep(1)
+                        st_frame = st.empty()
 
-                if start_camera and not stop_camera:
-                    self.cap = cv2.VideoCapture(0)
-                    time.sleep(1)
-                    st_frame = st.empty()
+                        while self.cap.isOpened():
+                            ret, frame = self.cap.read()
+                            if not ret:
+                                st.error(CAMERA_NOT_DETECTED_ISSUE)
+                                self.cap.release()
+                                break
 
-                    while self.cap.isOpened():
-                        ret, frame = self.cap.read()
-                        if not ret:
-                            st.error(CAMERA_NOT_DETECTED_ISSUE)
-                            self.cap.release()
-                            break
-
-                        frame = cv2.flip(frame, 1)
-                        result_img = self.comparison.practice_pose(ideal_frame, frame)
-                        st_frame.image(result_img, channels='BGR', use_column_width=True)
-
-                        if stop_camera:
-                            self.cap.release()
-                            break
-
+                            frame = cv2.flip(frame, 1)
+                            try: 
+                                result_img = self.comparison.practice_pose(ideal_frame, frame)
+                                st_frame.image(result_img, channels='BGR', use_column_width=True)
+                            except:
+                                st_frame.error(POSE_NOT_DETECTED_ISSUE)
+                            if stop_camera:
+                                self.cap.release()
+                                break
+        except:
+            st.error(CAMERA_ISSUE)
     def practice(self):
         st.title(PRACTICE_TITLE)
         practice_option = st.radio('Choose input option:', ["Upload Image", "Live Camera"])
